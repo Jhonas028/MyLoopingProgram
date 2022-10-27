474f0a92098fd89b3656bf4b622ad9fe4f65a68a
Jhonas02815 <soterojhonas028@gmail.com>
2022-10-27 21:56:19 +0800

    Write a program to calculate the sum of numbers from 1 to 20

---
+++ sumof1to20.java
@@ -0,0 +1,11 @@
+public class sumof1to20 {
+    public static void main(String args[]) {
+        int sum = 0;
+        
+        for(int i=1; i<=20; i++) {
+            sum = 3 + sum;
+        }
+        
+        System.out.print("The sum of 1 to 20 is " + sum);
+    }
+}
