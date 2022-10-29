e29b3be872d88aa6c689d52ee34e14682e21c9f9
Jhonas02815 <soterojhonas028@gmail.com>
2022-10-29 18:42:50 +0800

    You have to design the code such that the user has only three tries to guess the correct pin of the account. You set the pin as a constant with a final attribute. When correct display “Correct, welcome back.” When incorrect display “Incorrect, try again.”. When ran out of tries display “Sorry but you have been locked out.

--- passwordthreetries.java
+++ passwordthreetries.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class passwordthreetries {
+public class paswordthreetries {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         int pass = 123;
