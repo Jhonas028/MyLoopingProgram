180870c5a4f29741e7031b95df63218e9dd0369e
Jhonas02815 <soterojhonas028@gmail.com>
2022-11-17 15:31:59 +0800

    Seven days in Four weeks

---
+++ 4Weeks.java
@@ -0,0 +1,30 @@
+
+public class MyClass {
+    public static void main(String args[]) {
+      
+       System.out.println("Week 1\n");
+       for (int i = 1; i <= 7; i++) {
+           System.out.println("Day" + " " + i);
+       }
+       System.out.println("\n");
+       System.out.println("Week 2\n");
+       for (int i = 1; i <= 7; i++) {
+           System.out.println("Day" + " " + i);
+       }
+       System.out.println("\n");
+       System.out.println("Week 3\n");
+       for (int i = 1; i <= 7; i++) {
+           System.out.println("Day" + " " + i);
+       }
+       System.out.println("\n");
+       System.out.println("Week 4\n");
+       for (int i = 1; i <= 7; i++) {
+           System.out.println("Day" + " " + i);
+       }
+       
+
+       
+       
+       
+    }
+}
